dfeee1110953979b75d59dc0e4124fd61e38aeaa
Shammah1998 <96069439+Shammah1998@users.noreply.github.com>
2023-02-22 09:59:42 +0000

    Add files via upload

---
+++ getstarted.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Shammah Delivery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="getstarted.css">
+    
+  <body>
+    <header>
+      <nav>
+        <div class="logo">
+          <img src="companylogo.png"alt="Van logo" >
+          <li>
+              </div>
+              
+         <div class="form">
+                <form action="#" method="get">
+              <input type="text" name="search" placeholder="Search...">
+              <button type="submit">Search</button>
+            </form>
+            </li>
+        </div>
+    </div>
+    
+    <ul>
+         <li><a href="#">Cart</a></li>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="register.php">Sign in</a>/
+          <a href="register.php">Login</a>
+        </li>
+         </ul>
+      </nav>
+    </header>
+    
+   <main>
+   
+    bchjabjscbsuia asc aspiub c sjkn siaucbs c spib C JBDUBC EJHC SJB SJC CJBSBC DJC  N;Zns JA scuD8BDN SMK SDJ WUCBE NC AJScu bshc edjhc asjkC SUB WJLNSa;CNSWPACBCnuwijcb ejkd skncedbc
+   </main>
+    <div class="footer">
+    <footer>
+      <p>&copy; Shammah Delivery. All rights reserved.</p>
+    </footer>
+  </div>
+    <script src="script.js"></script>
+  </body>
+</html>
